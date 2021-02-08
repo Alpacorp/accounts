2a7da0cf400481b2eb4798a60fda9ef3b0022f7a
Alejandro Palacios Arévalo <alejandro.palacios88@gmail.com>
2021-02-08 03:56:34 -0500

    update general

--- database/connect.js
+++ database/connect.js
@@ -4,8 +4,6 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('mysql://root@localhost:8080/users_admin');
 
-// const sequelize = new Sequelize('mysql://bb7f72375032a7:5d91fd8c@us-cdbr-east-03.cleardb.com/heroku_f2dae762950c24e');
-
 // const mysql2 = require('mysql2');
 
 module.exports = {sequelize};
