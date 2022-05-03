6d8fb49c6738ccc01edc15ce240300f7b767be9a
unknown <alejandro.palacios88@gmail.com>
2022-05-03 15:21:16 -0500

    change var con

--- database/connect.js
+++ database/connect.js
@@ -6,7 +6,7 @@ const Sequelize = require("sequelize");
 // const sequelize = new Sequelize(process.env.CON3CTHE);
 
 const sequelize = new Sequelize(
-  "mysql://databaseKeys.:2d68da1d@us-cdbr-east-03.cleardb.com/heroku_cdadabb8d019873"
+  "mysql://be9347c52cb2f1:2d68da1d@us-cdbr-east-03.cleardb.com/heroku_cdadabb8d019873"
 );
 
 module.exports = { sequelize };
