10a72c0c663095150223eb14a9333afff0f9f83e
unknown <alejandro.palacios88@gmail.com>
2022-05-03 16:44:16 -0500

    test env var values

--- database/connect.js
+++ database/connect.js
@@ -3,10 +3,10 @@ const Sequelize = require("sequelize");
 
 // const sequelize = new Sequelize("mysql://root@localhost:3307/social_accounts");
 
-// const sequelize = new Sequelize(process.env.CON3CTHE);
+const sequelize = new Sequelize(process.env.CON3CTHE);
 
-const sequelize = new Sequelize(
-  "mysql://be9347c52cb2f1:2d68da1d@us-cdbr-east-03.cleardb.com/heroku_cdadabb8d019873"
-);
+// const sequelize = new Sequelize(
+//   "mysql://be9347c52cb2f1:2d68da1d@us-cdbr-east-03.cleardb.com/heroku_cdadabb8d019873"
+// );
 
 module.exports = { sequelize };
