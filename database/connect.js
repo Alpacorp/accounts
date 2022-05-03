fce601f9632db0856142b0888dd25fe2155fa2af
unknown <alejandro.palacios88@gmail.com>
2022-05-03 15:10:50 -0500

    change and configure env vars

--- database/connect.js
+++ database/connect.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 
 // const sequelize = new Sequelize("mysql://root@localhost:3307/social_accounts");
 
-const sequelize = new Sequelize(process.env.CON3CT);
+const sequelize = new Sequelize(process.env.CON3CTHE);
 
 // const sequelize = new Sequelize(
 //   "mysql://databaseKeys.:2d68da1d@us-cdbr-east-03.cleardb.com/heroku_cdadabb8d019873"
